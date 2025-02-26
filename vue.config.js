faea29bba5314270862e0c82e43b6cdb8d74959f
Mohammad Alnady <mohammad.alnady@wilketechnology.local>
2025-02-26 13:24:34 +0100

    Verschiebe Inhalte von gh-pages nach main

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     port: 8080,
-    open: true
-  }
-}
+    open: true,
+  },
+};
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Mohalnady/" : "/",
+};
